4b22096577506246a71b067b69ef6983fd1a41f6
simodevjs <simodevjs@gmail.com>
2024-10-15 04:38:32 +0100

    ijijij

---
+++ quiz.js
@@ -0,0 +1,14 @@
+var bitina = document.getElementById("submit-answer");
+bitina.onclick = function checkAnswer() {
+    var correctAnswer = 4;
+    var userAnswer = document.querySelector('input[name="quiz"]:checked').value;
+    if (userAnswer == correctAnswer) {
+        document.getElementById("feedback").textContent = "Correct! Well done.";
+    } else {
+        document.getElementById("feedback").textContent = "That's incorrect. Try again!";
+    }
+}
+
+
+
+/* document.getElementById("submit-answer").addEventListener("click", checkAnswer); */
